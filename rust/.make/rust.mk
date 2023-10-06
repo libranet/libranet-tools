5790945b7cd8ce7e11716956aa1dd81465d0ab05
Wouter Vanden Hove <wvandenhove@ekglobal.co>
2023-10-07 01:57:21 +0200

    update

--- rust/.make/rust.mk
+++ rust/.make/rust.mk
@@ -1,25 +1,31 @@
 .PHONY: all
 all: test
 
+
 .PHONY: build
 build:
 	@cargo build --all
+
 
 .PHONY: test
 test:
 	@cargo test --all
 
+
 .PHONY: check
 check:
 	@cargo check --all
+
 
 .PHONY: format
 format:
 	@cargo fmt --all
 
+
 .PHONY: format-check
 format-check:
 	@cargo fmt --all -- --check
+
 
 .PHONY: lint
 lint:
