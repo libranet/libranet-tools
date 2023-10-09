4078405fdc77fe3467539c09951ad3ce307f6bed
Wouter Vanden Hove <wouter@wvhconsulting.org>
2023-10-10 01:11:27 +0200

    update

--- jenkins/bin/jenkins-start.sh
+++ jenkins/bin/jenkins-start.sh
@@ -2,9 +2,10 @@
 
 # This script could be symlinked from another location.
 THIS_DIR=$(readlink -f $(dirname $(readlink -f $0)))
-
-export JAVA_HOME=/path/to/your/java-11-directory
-export PATH=$JAVA_HOME/bin:$PATH
 export TMP={THIS_DIR}/var/tmp
 
-java -jar var/jenkins.war
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
+export PATH=$JAVA_HOME/bin:$PATH
+
+export JENKINS_HOME="/opt/tools/jenkins/var/home"
+java -jar ${THIS_DIR}/../jenkins.war
