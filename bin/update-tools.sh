61abb83fcba69e4a903789c7f322fb0b6e515391
Wouter Vanden Hove <wouter@wvhconsulting.org>
2023-10-29 15:54:04 +0100

    update

---
+++ bin/update-tools.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+cd /opt/tools/black && make update
+echo -e "\n\n"
+
+cd /opt/tools/ruff && make update
+echo -e "\n\n"
+
+cd /opt/tools/isort && make update
+echo -e "\n\n"
+
+
+# Define a function to update a tool
+update_tool() {
+  tool_dir="$1"
+  (cd "$tool_dir" && make update)
+}
+
+# List of tool directories to update
+tool_directories=(
+  "/opt/tools/black"
+  "/opt/tools/ruff"
+  "/opt/tools/isort"
+)
+
+# Use parallel to execute the update_tool function for each tool directory
+# export -f update_tool
+
+# Use parallel to execute the update_tool function for each tool directory
+# parallel -j4 update_tool ::: "${tool_directories[@]}"
