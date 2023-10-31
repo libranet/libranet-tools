d56c29cff00d305023a7dd86a3ae6eea7c49ef66
Wouter Vanden Hove <wouter@wvhconsulting.org>
2023-10-31 22:07:35 +0100

    update

--- bin/update-tools.sh
+++ bin/update-tools.sh
@@ -4,12 +4,27 @@
 cd /opt/tools/black && make update
 echo -e "\n\n"
 
-cd /opt/tools/ruff && make update
-echo -e "\n\n"
-
 cd /opt/tools/isort && make update
 echo -e "\n\n"
 
+cd /opt/tools/pre-commit && make update
+echo -e "\n\n"
+
+cd /opt/tools/pyenv && make update
+echo -e "\n\n"
+
+cd /opt/tools/ruff && make update
+echo -e "\n\n"
+
+cd /opt/tools/twine && make update
+echo -e "\n\n"
+
+cd /opt/tools/xdg-ninja && make update
+echo -e "\n\n"
+ 
+cd /opt/tools/zestreleaser && make update
+echo -e "\n\n"
+ 
 
 # Define a function to update a tool
 update_tool() {
