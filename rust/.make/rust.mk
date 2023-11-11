3c77817aa10b684d06cb9dd4af8b7281395aa7e6
Wouter Vanden Hove <wouter@wvhconsulting.org>
2023-11-12 00:12:04 +0100

    update

--- rust/.make/rust.mk
+++ rust/.make/rust.mk
@@ -1,32 +1,19 @@
-.PHONY: all
-all: test
 
 
-.PHONY: build
 build:
 	@cargo build --all
 
-
-.PHONY: test
 test:
 	@cargo test --all
 
-
-.PHONY: check
 check:
 	@cargo check --all
 
-
-.PHONY: format
 format:
 	@cargo fmt --all
 
-
-.PHONY: format-check
 format-check:
 	@cargo fmt --all -- --check
 
-
-.PHONY: lint
 lint:
 	@cargo clippy --all -- -D clippy::dbg-macro -D warnings
