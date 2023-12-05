6f22d2c12d2598042d15c4c3185a6e3b1d1cf5de
Wouter Vanden Hove <wouter@wvhconsulting.org>
2023-12-05 15:07:39 +0100

    update

--- terraform/etc/env.sh
+++ terraform/etc/env.sh
@@ -1,3 +1,4 @@
 # https://developer.hashicorp.com/terraform/cli/config/environment-variables
-export TF_CLI_CONFIG_FILE="/opt/tools/teeraform/etc/terraformrc"
-export TF_PLUGIN_CACHE_DIR="/opt/tools/teeraform/var/cache/"
+export TF_CLI_CONFIG_FILE="/opt/tools/terraform/etc/terraformrc"
+export TF_PLUGIN_CACHE_DIR="/opt/tools/terraform/var/cache/terraform-plugin-cache"
+export TFENV_CONFIG_DIR="/opt/tools/terraform/var/tfenv"
